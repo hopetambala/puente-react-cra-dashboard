0ac94a46a96f70f2f74c56670000809cf23919d3
Hope Tambala <hopetambala@outlook.com>
2019-04-29 01:47:32 -0400

    refactor(): use final-forms

--- src/js/pages/DataExport.js
+++ src/js/pages/DataExport.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
+import { Button,Row,Col } from 'react-bootstrap'
+import { ApolloConsumer } from 'react-apollo';
+import { Form, Field } from 'react-final-form';
+
+
+//Components
 import { ResultsTable } from '../components/widget/Table/Table';
 
+//Queries
+import { all_records } from '../queries/records';
+
+
+//Styling
+import Styles from '../components/styles/Styles'
 import 'bootstrap/dist/css/bootstrap.css';
 
 const styles = {
@@ -11,7 +22,7 @@ const styles = {
 		justifyContent: 'center',
 		//alignItems: 'flex-center',
 		alignContent: 'flex-start',
-		paddingTop: '2.5%'
+		paddingTop: '5%'
 		
 	},
 	row: {
@@ -23,12 +34,68 @@ const styles = {
 	}
   }
 
+  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+  const onSubmit = async values => {
+	await sleep(300)
+	window.alert(JSON.stringify(values, 0, 2))
+  }
+
 export class ExportPage extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			type:null,
+			org:null
+
+		}
+	}
+
 	render() {
 		return (
-			<div style={styles.container}>
-				<ResultsTable />
-			</div>
+			<Styles style={styles.container}>
+			<h1>🏁 Data Exporter</h1>
+			<Form
+				onSubmit={onSubmit}
+				initialValues={{ type: 'all', organization: '' }}
+				render={({ handleSubmit, form, submitting, pristine, values }) => (
+				<form onSubmit={handleSubmit}>
+				<div>
+					<label>Record Type</label>
+					<Field name="type" component="select">
+						<option />
+						<option value="dem">Demographics Only</option>
+						<option value="med">Dem + Medical Evaluation</option>
+						<option value="env">Dem + Environmental Health</option>
+					</Field>
+				</div>
+				<div>
+					<label>Toppings</label>
+					<Field name="organization" component="select" >
+						<option />
+						<option value="Puente">Puente</option>
+						<option value="One World Surgery">One World Surgery</option>
+						<option value="WOF">World Outreach Foundation</option>
+					</Field>
+				</div>
+				
+				<div className="buttons">
+					<button type="submit" disabled={submitting || pristine}>
+						Submit
+					</button>
+
+					<button
+						type="button"
+						onClick={form.reset}
+						disabled={submitting || pristine}>
+						Reset
+					</button>
+				</div>
+				<pre>{JSON.stringify(values, 0, 2)}</pre>
+				</form>
+			)}
+			/>
+		</Styles>
 		);
 	}
 }
