59f80e61c78cf0127b96d7ae2c76fef851e3f7da
Hope Tambala <hopetambala@outlook.com>
2019-04-29 02:01:36 -0400

    style(): navbar

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -8,25 +8,34 @@ import { ExportPage } from "../pages/DataExport";
 import { MedicalEvalAnalytics} from '../pages/MedicalEval';
 import { EnvironHealthAnalytics } from '../pages/EnvironHealth';
 
+//Styling
+import styled from 'styled-components'
+
+const StyledNav = styled(Nav)`
+    //&:hover {
+	background: #1a2a6c !important;
+	color: #f8af1e
+	//}
+`;
 
 export default class Layout extends React.Component {
 	render() {
 		return (
 				<Router>
-					<Nav style={{background:'white'}} className="navbar navbar-expand-lg fixed-top is-white is-dark-text">
+					<StyledNav  className="navbar navbar-expand-lg fixed-top is-white is-dark-text bg-light">
 						<div className="navbar-brand h1 mb-0 text-large font-medium">
-							Puente
+							PUENTE
 						</div>
 						<div className="navbar-nav ml-auto">
-							<span className="pr-2"><Link to="/">Dashboard</Link></span>
-							<span className="pr-2">Maps</span>
-							<span className="pr-2"><Link to="/dataexport">Export Manager</Link></span>
+							<span className="pr-2"><Link to="/">DASHBOARD</Link></span>
+							<span className="pr-2">MAPS</span>
+							<span className="pr-2"><Link to="/dataexport">EXPORT MANAGER</Link></span>
 							<span className="pr-2">Hi, {this.props.username}</span>
 							<span className="img-container">
 								{/*<img src={myMan} className="rounded-circle" alt="user" /> */}
 							</span>
 						</div>	
-					</Nav>
+					</StyledNav>
 					
 					<Route exact path="/" component={HomePage} />
 					<Route path="/dataexport" component={ExportPage} />
