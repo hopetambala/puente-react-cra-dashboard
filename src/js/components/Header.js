852f31ec6468946bb7bca1154aa3d0773d926da8
Hope Tambala <hopetambala@outlook.com>
2019-04-29 23:55:25 -0400

    refactor(): unnecessary imports

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import {PropTypes } from 'prop-types'
-import { Button,Col, Row,Card, Nav} from 'react-bootstrap';
-import myMan from '../../assets/man.png'
+import { Nav } from 'react-bootstrap';
 
 
 export default class Header extends React.Component {
