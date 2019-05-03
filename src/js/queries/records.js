bb13c8b687e7d76cd429f3803c9d764de050bd45
Hope Tambala <hopetambala@outlook.com>
2019-05-03 01:05:06 -0400

    feat(): add all organizations queries

--- src/js/queries/records.js
+++ src/js/queries/records.js
@@ -71,3 +71,137 @@ export const vitals = gql`
         }
     }  
 `;
+
+export const allRecordsByOrganization = gql`
+    query($organization: String!){
+        getPeopleByOrganization(organization: $organization){
+            fname
+            lname
+            nickname
+            sex
+            dob
+            telephoneNumber
+            educationLevel
+            occupation
+            city
+            province
+            clinicProvider
+            cedulaNumber
+            surveyingUser
+            surveyingOrganization
+            latitude
+            longitude
+        }
+    }
+`;
+export const allVitalsByOrganization = gql`
+    query($organization: String!){
+        getVitalByOrganization(organization: $organization){
+            fname
+            lname
+            nickname
+            sex
+            dob
+            telephoneNumber
+            educationLevel
+            occupation
+            city
+            province
+            clinicProvider
+            cedulaNumber
+            surveyingUser
+            surveyingOrganization
+            latitude
+            longitude
+
+            sex
+            dob
+            bmi
+            bloodSugar
+            bloodPressure
+            Systolic
+            Diastolic
+        }
+    }  
+`;
+
+export const allEnvsByOrganization = gql`
+    query($organization: String!){
+        getEnvByOrganization(organization: $organization){
+            fname
+            lname
+            nickname
+            sex
+            dob
+            telephoneNumber
+            educationLevel
+            occupation
+            city
+            province
+            clinicProvider
+            cedulaNumber
+            surveyingUser
+            surveyingOrganization
+            latitude
+            longitude
+            yearsLivedinthecommunity
+            yearsLivedinThisHouse
+            waterAccess
+            typeofWaterdoyoudrink
+            latrineAccess
+            clinicAccess
+            conditionoRoofinyourhouse
+            conditionoRoofinyourhouse
+            medicalproblemswheredoyougo
+            dentalproblemswheredoyougo
+            biggestproblemofcommunity
+            timesperweektrashcollected
+            wheretrashleftbetweenpickups
+            numberofIndividualsLivingintheHouse
+            numberofChildrenLivinginHouseUndertheAgeof5
+            houseownership
+        }
+    }  
+`;
+
+export const allEvalMedicalsByOrganization = gql`
+    query($organization: String!){
+        getEvalMedicalByOrganization(organization: $organization){
+            fname
+            lname
+            nickname
+            sex
+            dob
+            telephoneNumber
+            educationLevel
+            occupation
+            city
+            province
+            clinicProvider
+            cedulaNumber
+            surveyingUser
+            surveyingOrganization
+            latitude
+            longitude
+            
+            chronic_condition_diabetes
+            chronic_condition_hypertension
+            chronic_condition_other
+            seen_doctor
+            received_treatment_notes
+            received_treatment_description
+            receiving_treatment_support_required
+            part_of_body
+            part_of_body_description
+            duration
+            trauma_induced
+            condition_progression
+            notes
+            AssessmentandEvaluation
+            AssessmentandEvaluation_Surgical
+            AssessmentandEvaluation_Surgical_Guess
+            immediate_follow_up
+            planOfAction
+        }
+    }  
+`;
