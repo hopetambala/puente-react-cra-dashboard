b929d5cb3f5a3a1d470d9c89d9eaf4eb40210113
Hope Tambala <hopetambala@outlook.com>
2019-05-18 16:42:55 -0400

    fix(): withApollo

--- src/js/pages/EnvironHealth.js
+++ src/js/pages/EnvironHealth.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Row, Container } from 'react-bootstrap';
-import { Query, withApollo } from 'react-apollo';
+import { Query, withApollo, graphql } from 'react-apollo';
 
 //Components
-import { Boxx } from '../components/widget/Boxx/Boxx';
+import { StatsBox } from '../components/widget/StatsBox/StatsBox';
 import { ThreeDimenEnvComponent } from '../components/recharts/Scatter_Env';
 
 //Query
-import { env_age_latrines_clinic as ealc} from '../queries/records';
+import { env_age_latrines_clinic as ealc, allEnvsByOrganization} from '../queries/records';
 
 
 const styles = {
@@ -30,37 +30,55 @@ const styles = {
 }
 
 
-export class EnvironHealthAnalytics extends React.Component {
+class EnvironHealthAnalytics extends React.Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			data:null
+			results:null,
+			organization:"Puente"
 		}
+		
+	}
+	/*https://github.com/apollographql/react-apollo/issues/1411*/
+	componentWillMount = async () => {
+		const {client} = this.props;
+		let res = await client.query({query: allEnvsByOrganization,variables: {organization:this.state.organization }});
+		this.setState({results: res.data.getEnvByOrganization})
+		console.log(this.state.results);
 	}
 
-	
+	componentDidMount(){
+		
+	}
 
+	dataWrangle(){
+		var data = this.state.results
 
+	}
 	render() {
 		return (
-				<Container style={styles.container}>
-					{/*<Row style={styles.row}>
-						<Boxx/>
-						<Boxx/>
-					</Row>*/}
-					<Row style={styles.row}>
-						<Query query={ealc}>
-							{({ data, loading, error }) => {
-								if (loading) return <p>Loading...</p>;
-								if (error) return <p>Error :(</p>;
-								//console.log(data.getEvalMedicalRecords);
-								return (
-									<ThreeDimenEnvComponent data={data} />
-								);
-							}}
-						</Query>
-					</Row>
-				</Container>		
+			<Container style={styles.container}>
+				<Row style={styles.row}>
+				{/**
+					<StatsBox/>
+					<StatsBox/>  */}
+				</Row>
+				<Row style={styles.row}>
+					<Query query={ealc}>
+						{({ data, loading, error }) => {
+							if (loading) return <p>Loading...</p>;
+							if (error) return <p>Error :(</p>;
+							//console.log(data.getEvalMedicalRecords);
+							return (
+								<ThreeDimenEnvComponent data={data} />
+							);
+						}}
+					</Query>
+				</Row>
+			</Container>
 		);
 	}
 }
+
+export default withApollo(EnvironHealthAnalytics);
+ 
