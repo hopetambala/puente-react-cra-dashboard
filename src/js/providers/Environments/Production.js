94415f4b01d1785cdba758dc7bf03c61fa2f5041
Hope Tambala <hopetambala@outlook.com>
2019-06-25 19:56:55 -0400

    chore(): /:

--- src/js/providers/Environments/Production.js
+++ src/js/providers/Environments/Production.js
@@ -1,6 +1,6 @@
 export const ENV = {
-    parseAppId :  'vBdTHqQU31IyLW5uYRDIWb8Ew8zCZGBzMqChugjr',
-    parseJavascriptKey : 'jFWiqliNRHXiH72J9kiotL8m0EuSdry1yFIiYlad',
-    parseServerUrl :  'https://parseapi.back4app.com/',
-    parseMasterKey :  'Mmsy8ZiqilnYG81lI7T4GhfwP4jbsdAtjBYtu3uC'
+    parseAppId :  process.env.APPID,
+    parseJavascriptKey : process.env.JSKEY,
+    parseServerUrl :  process.env.SERVERURL,
+    parseMasterKey :  process.env.MASTERKEY
 }
