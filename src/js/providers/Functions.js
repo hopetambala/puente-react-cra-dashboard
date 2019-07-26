a0b00f6d781f2d5fe2182ecd6761e1004edf9d37
Hope Tambala <hopetambala@outlook.com>
2019-07-26 11:36:21 -0400

    fix(): add unittests and fix getAge function

--- src/js/providers/Functions.js
+++ src/js/providers/Functions.js
@@ -1,8 +1,8 @@
 
-function get_age(time){
+function get_age(time, start_date){
     var MILLISECONDS_IN_A_YEAR = 1000*60*60*24*365;
     var date_array = time.split('-')
-    var years_elapsed = (new Date() - new Date(date_array[0],date_array[1],date_array[2]))/(MILLISECONDS_IN_A_YEAR);
+    var years_elapsed = (start_date - new Date(date_array[0],date_array[1],date_array[2]))/(MILLISECONDS_IN_A_YEAR);
     return parseInt(years_elapsed); 
 }
 
