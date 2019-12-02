f0173918e32ffb7bad1eec58d2c61c0cafd94d07
Hope Tambala <hopetambala@outlook.com>
2019-12-02 11:39:00 -0500

    feat: custom form data retrieval

--- src/js/pages/DataExport.js
+++ src/js/pages/DataExport.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { Button } from 'react-bootstrap'
-import { Query } from 'react-apollo';
+import { Query, withApollo} from 'react-apollo';
+import { useQuery } from '@apollo/react-hooks';
 import { Form, Field } from 'react-final-form';
 
 // Styles
@@ -18,7 +19,10 @@ import {
 	allEnvsByOrganization, 
 	allVitalsByOrganization,
 	allEvalMedicalsByOrganization,
-	allHistoryMedicalsByOrganization} from '../queries/records';
+	allHistoryMedicalsByOrganization,
+	allCustomSpecs,
+	allCustomResultsByFormId
+} from '../queries/records';
 
 import { CSVLink } from "react-csv";
 
@@ -27,27 +31,6 @@ import { CSVLink } from "react-csv";
 //Styling
 import Styles from '../components/styles/Styles'
 import 'bootstrap/dist/css/bootstrap.css';
-
-// const styles = {
-// 	container: {
-// 		flexDirection: 'row',
-// 		flexWrap: 'wrap',
-// 		justifyContent: 'center',
-// 		//alignItems: 'flex-center',
-// 		alignContent: 'flex-start',
-// 		paddingTop: '5%'
-// 	},
-// 	row: {
-// 		//height:'100vh',	
-// 		justifyContent: 'center',
-// 		flex:1,
-// 		marginBottom:0,
-// 		paddingBottom:0
-// 	},
-// 	button: {
-// 		backgroundColor:'white'
-// 	}
-//   }
 
 const Dem = ({ organization }) => (
 	<Query
@@ -178,12 +161,106 @@ const HistoryMedical = ({ organization }) => (
 	</Query>
 );
 
+// function CustomData({id}) {
+// 	const { loading, error, data, networkStatus } = useQuery(allCustomResultsByFormId, {
+// 		variables: { id },
+// 	})
+
+// 	useEffect(() => {
+// 		async function cleanData(data) {
+// 			let cleaned_data = data;
+// 			for (let i = 0; i < cleaned_data['getCustomFormResultsbyId'].length; i++) {
+// 				for (let j = 0; j < cleaned_data['getCustomFormResultsbyId'][i]['fields'].length; j++){
+// 					let question = cleaned_data['getCustomFormResultsbyId'][i]['fields'][j].title
+// 					let answer  = cleaned_data['getCustomFormResultsbyId'][i]['fields'][j].answer
+// 					cleaned_data['getCustomFormResultsbyId'][i][question] = await answer
+// 				}
+// 			}
+// 			return(cleaned_data)
+// 		}
+// 		cleanData(data);
+// 	 }, [])
+
+// 	// for (let i = 0; i < data['getCustomFormResultsbyId'].length; i++) {
+// 	// 	for (let j = 0; j < data['getCustomFormResultsbyId'][i]['fields'].length; j++){
+// 	// 		let question = data['getCustomFormResultsbyId'][i]['fields'][j].title
+// 	// 		let answer  = data['getCustomFormResultsbyId'][i]['fields'][j].answer
+// 	// 		data['getCustomFormResultsbyId'][i][question] = answer
+// 	// 	}
+// 	// }
+
+// 	if (networkStatus === 4) return "Refetching!";
+// 	if (loading) return <LoadingDots />;
+// 	if (error) return `Error!: ${error}`;
+
+// 	return(
+// 		<>
+// 		<Button style={styles.button}>
+// 		{console.log(data)}
+// 			<CSVLink data={data['getCustomFormResultsbyId']}>
+// 				Download
+// 			</CSVLink>
+// 		</Button>
+// 		<DataTable data={data['getCustomFormResultsbyId']} />
+// 		</>) 	
+// }
+
+ class CustomData extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { 
+			data: null 
+		};
+	}
+
+	async clean_data(data){
+		var cleaned_data = await data;
+		for (let i = 0; i < cleaned_data['getCustomFormResultsbyId'].length; i++) {
+			for (let j = 0; j < cleaned_data['getCustomFormResultsbyId'][i]['fields'].length; j++){
+				var question = String(cleaned_data['getCustomFormResultsbyId'][i]['fields'][j].title)
+				var question = question.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+				var answer  =  String(cleaned_data['getCustomFormResultsbyId'][i]['fields'][j].answer)
+				var answer = answer.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+				cleaned_data['getCustomFormResultsbyId'][i][question] = answer
+			}
+			 delete cleaned_data['getCustomFormResultsbyId'][i]['fields']
+		}
+		console.log(cleaned_data)
+		this.setState({
+			data:cleaned_data
+		})
+	}
+
+	componentDidMount = async() => {
+		const { client } = this.props;
+		let res = await client.query({query: allCustomResultsByFormId, variables: {id: this.props.id }});
+		await this.clean_data(res.data);
+	}
+
+	render() {
+		return(
+			<>
+			{this.state.data !== null &&
+			<>
+			<Button style={styles.button}>
+				<CSVLink data={this.state.data['getCustomFormResultsbyId']}>
+					Download
+				</CSVLink>
+			</Button>
+			<DataTable data={this.state.data['getCustomFormResultsbyId']} />
+			</>
+			}
+			</>);
+		
+	}
+  }
+
 export class ExportPage extends React.Component {
 	constructor(props){
 		super(props)
 		this.state = {
 			type:"Demographics",
-			org:'Puente'
+			org:'Puente',
 		}
 	}
 	
@@ -191,6 +268,7 @@ export class ExportPage extends React.Component {
 		await this.setState({
 			type: values.type,
 			org: values.organization,
+			formId: values.formId
 		})
 	}
 	
@@ -212,6 +290,10 @@ export class ExportPage extends React.Component {
 		else if (this.state.type === "Medical History") {
 			aThing = <HistoryMedical organization={this.state.org} />;
 		}
+		else if (this.state.type === "Custom") {
+			const CustomWithApollo = withApollo(CustomData);
+			aThing = <CustomWithApollo id={this.state.formId} />;
+		}
 
 
 
@@ -225,16 +307,6 @@ export class ExportPage extends React.Component {
 				render={({ handleSubmit, form, submitting, pristine, values }) => (
 				<form onSubmit={handleSubmit} style={{backgroundColor: styles.theme.lighter_darkbg}} >
 				<div>
-					<label>Record Type</label>
-					<Field name="type" component="select">
-						<option value="Demographics">Demographics Only</option>
-						<option value="Medical Evaluation">Dem + Medical Evaluation</option>
-						<option value="Environmental Health">Dem + Environmental Health</option>
-						<option value="Vitals">Dem + Vitals</option>
-						<option value="Medical History">Dem + Medical History</option>
-					</Field>
-				</div>
-				<div>
 					<label>Organizations</label>
 					<Field name="organization" component="select" >
 						<option ></option>
@@ -245,6 +317,44 @@ export class ExportPage extends React.Component {
 						<option value="DR Missions">DR Missions & Good Samaritan</option>
 					</Field>
 				</div>
+				<div>
+					<label>Record Type</label>
+					<Field name="type" component="select">
+						<option value="Demographics">Demographics Only</option>
+						<option value="Medical Evaluation">Dem + Medical Evaluation</option>
+						<option value="Environmental Health">Dem + Environmental Health</option>
+						<option value="Vitals">Dem + Vitals</option>
+						<option value="Medical History">Dem + Medical History</option>
+						<option value="Custom">Custom Forms</option>
+					</Field>
+				</div>
+				{values.type === 'Custom' &&
+					<div>
+					<label>Forms</label>
+					<Field name="formId" component="select" >
+						<Query
+							query={allCustomSpecs}
+							// variables={{ organization }}
+							notifyOnNetworkStatusChange
+						>
+							{({ loading, error, data, networkStatus }) => {
+							if (networkStatus === 4) return "Refetching!";
+							if (loading) return <LoadingDots />;
+							if (error) return `Error!: ${error}`;
+
+							return (
+								<>
+								<option ></option>
+									{data.getCustomFormSpec.map((opt) => {
+										return <option key={opt.objectId} value={opt.objectId}>{opt.title}</option>
+									})} 
+								</>
+							);
+							}}
+						</Query>
+					</Field>
+					</div>
+				}
 				
 				<div className="buttons">
 					<button type="submit" disabled={submitting || pristine}>
@@ -273,3 +383,5 @@ export class ExportPage extends React.Component {
 		);
 	}
 }
+
+// export default withApollo(CustomData);
